0a641d84d362924f2d490c5eb1a831a060b84f2b
Benjamin Fallon <benjamin.a.fallon@gmail.com>
2015-12-08 19:23:33 -0500

    Add simple ListView

--- app/src/main/java/com/benjaminafallon/androidapps/citydist/MainActivity.java
+++ app/src/main/java/com/benjaminafallon/androidapps/citydist/MainActivity.java
@@ -2,6 +2,12 @@ package com.benjaminafallon.androidapps.citydist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+
+        ListView listView = (ListView) findViewById(R.id.mainListView);
+        listView.setAdapter(itemsAdapter);
+
     }
+
 }
