74999dc86e4af8cc00982d63c16c01e5baea0387
Benjamin Fallon <benjamin.a.fallon@gmail.com>
2015-12-08 22:30:24 -0500

    Connect to Google Play Services location APIs

--- app/src/main/java/com/benjaminafallon/androidapps/citydist/MainActivity.java
+++ app/src/main/java/com/benjaminafallon/androidapps/citydist/MainActivity.java
@@ -1,5 +1,9 @@
 package com.benjaminafallon.androidapps.citydist;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +14,28 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    GoogleApiClient mGoogleApiClient = null;
+    Location mLastLocation;
+    TextView textViewLong;
+    TextView textViewLat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewLat = (TextView) findViewById(R.id.textViewLat);
+        textViewLong = (TextView) findViewById(R.id.textViewLong);
+
 
         String[] values = new String[] { "New York", "Los Angeles", "Chicago" };
 
@@ -43,6 +61,45 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.mainListView);
         listView.setAdapter(itemsAdapter);
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            textViewLat.setText("Lat: " + String.valueOf(mLastLocation.getLatitude()));
+            textViewLong.setText("Long: " + String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
